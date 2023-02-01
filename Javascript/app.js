7dd6b8307600c814c7ba24a99854c4025aae7625
Davidkhvedelidze <122465978+Davidkhvedelidze@users.noreply.github.com>
2023-02-01 17:04:12 +0400

    Add files via upload

---
+++ Javascript/app.js
@@ -0,0 +1,16 @@
+// const suppurt = function () {
+//   console.log("new idea");
+// };
+// suppurt();
+
+// function sum(a) {
+//   return a + 20;
+// }
+
+// const sum = (a, b) => b * a + 10;
+// console.log(sum(15, 10));
+
+const sample = (x) => {
+  return Boolean(x !== 10);
+};
+console.log(sample("10"));
