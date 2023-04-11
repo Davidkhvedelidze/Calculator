483d5e8ab51cc42fefce7db0581354bdcfe7aeb6
David <datoxvedelidzee@gmail.com>
2023-04-11 15:41:06 +0400

    Calculator

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,16 +1,86 @@
-// const suppurt = function () {
-//   console.log("new idea");
-// };
-// suppurt();
+// const screen = document.querySelector(".screen");
+// const clearAll = document.querySelector("#clearAll");
+// const clear = document.querySelector("#clear");
+// const equal = document.querySelector("#equal");
+// const operators = document.querySelectorAll(".operator");
+// const
 
-// function sum(a) {
-//   return a + 20;
-// }
+// console.log(screen);
 
-// const sum = (a, b) => b * a + 10;
-// console.log(sum(15, 10));
+// clear.addEventListener("click", function () {
+//   screen.value = screen.value?.slice(0, -1);
+// });
+// clearAll.addEventListener("click", () => {
+//   screen.value = "";
+// });
 
-const sample = (x) => {
-  return Boolean(x !== 10);
-};
-console.log(sample("10"));
+// let numsToOperate = [];
+// let operator = "";
+// let num1;
+
+// number.forEach((num) => {
+//   num.addEventListener("click", function () {
+//     display.value += this.textContent;
+//     if (numsToOperate.length <= 1) {
+//       numsToOperate.push(display.value);
+//     } else {
+//       return;
+//     }
+//   });
+// });
+
+// operators.forEach((op) => {
+//   op.addEventListener("click", function () {
+//     if (op.innerHTML !== "=") {
+//       operator = op.innerHTML;
+//     }
+
+//     screen.value = " ";
+//   });
+// });
+
+// equal.addEventListener("click", function () {
+//   let num1 = Number(numsToOperate[0]);
+//   let num2 = Number(numsToOperate[1]);
+//   let result;
+//   switch (operator) {
+//     case "+":
+//       result = num1 + num2;
+//       break;
+//     case "-":
+//       result = num1 - num2;
+//       break;
+//     case "*":
+//       result = num1 * num2;
+//       break;
+//     case "/":
+//       result = num1 / num2;
+//       break;
+//   }
+
+//   screen.value = result;
+//   numsToOperate = [];
+// });
+
+const display = document.querySelector("#input-field");
+const button = document.querySelectorAll("button");
+
+button.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (btn.id === "equal") {
+      display.value = eval(display.value.replace(",", ".")).toLocaleString();
+    } else if (btn.id === "AC") {
+      display.value = "";
+    } else if (btn.id == "C") {
+      display.value = display.value.slice(0, -1);
+    } else {
+      if (btn.id === ".") {
+        if (!display.value.includes(".")) {
+          display.value += ".";
+        }
+      } else {
+        display.value += btn.id;
+      }
+    }
+  });
+});
